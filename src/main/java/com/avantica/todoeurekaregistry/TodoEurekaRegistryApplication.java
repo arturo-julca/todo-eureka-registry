e748965c1f6fe943b12545ffc1b9e1d406178fca
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-11 12:56:35 -0500

    First commit.

---
+++ src/main/java/com/avantica/todoeurekaregistry/TodoEurekaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.avantica.todoeurekaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoEurekaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoEurekaRegistryApplication.class, args);
+	}
+
+}
